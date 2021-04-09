f4549cc567898acbe0054658ae35fe99bba30e7e
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-04-09 17:31:36 +0200

    Click

--- js/main.js
+++ js/main.js
@@ -2,7 +2,28 @@ $(document).ready(function(){
 
 
 
+var dropDown = $('.dropdown');
+var dropLink = dropDown.children('a');
+var dropMenu = dropDown.children('.dropmenu'); 
 
+
+//CLICK
+dropLink.click(function(){
+     
+   
+    var actualMenu = $(this).next()
+    //RESET 
+    dropMenu.not(actualMenu).hide('.dropmenu');
+
+    actualMenu.toggle();
+});
+
+//HOVER
+dropLink.hover(function(){
+
+    //CLICK
+
+});
 
 
 
